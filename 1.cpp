c0cd89a8dd01e026e38a0b479f347e59055c5c2a
Sofia-Gavrina <gavrinasofa229@gmail.com>
2025-05-30 14:59:50 +0300

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,37 +1,50 @@
-#include <iostream>
+﻿#include <iostream>
 #include <cmath>
-#include <iomanip>
-
 using namespace std;
 
-int main() {
-    // Заданные константы
+/**
+ * @brief Функция для вычисления значения переменной a по заданным константам.
+ * @param x значение константы.
+ * @param y значение константы.
+ * @param z значение константы.
+ * @return Результат вычисления переменной a.
+ */
+double calculateA(const double x, const double y, const double z);
+
+/**
+ * @brief Функция для вычисления значения переменной b по заданным константам.
+ * @param x значение константы.
+ * @param y значение константы.
+ * @param z значение константы.
+ * @return Результат вычисления переменной b.
+ */
+double calculateB(const double x, const double y, const double z);
+
+/**
+ * @brief Точка входа в программу
+ * @return 0 - если программа выполнена корректно, иначе - 1
+ */
+int main()
+{
     const double x = 0.5;
     const double y = 0.05;
     const double z = 0.7;
 
-    // Вычисление значения a
-    double numerator_a = pow(x, 2) * (x + 1);
-    double denominator_a = y - pow(sin(x + z), 2);
-    double a = numerator_a / denominator_a;
-
-    // Вычисление значения b
-    double sqrt_term = sqrt((x * y) / z);
-    double cos_term = pow(cos(pow(x + y, 2)), 2);
-    double b = sqrt_term + cos_term;
-
-    // Вывод исходных данных
-    cout << "Исходные данные:\n";
-    cout << fixed << setprecision(2);
-    cout << "x = " << x << "\n";
-    cout << "y = " << y << "\n";
-    cout << "z = " << z << "\n\n";
-
-    // Вывод результатов с повышенной точностью
-    cout << "Результаты вычислений:\n";
-    cout << fixed << setprecision(6);
-    cout << "a = " << a << "\n";
-    cout << "b = " << b << endl;
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+    cout << "z = " << z << endl;
+    cout << "a = " << calculateA(x, y, z) << endl;
+    cout << "b = " << calculateB(x, y, z) << endl;
 
     return 0;
+}
+
+double calculateA(const double x, const double y, const double z)
+{
+    return double(pow(x, 2) * (x + 1)) / (y - pow(sin(x + z), 2));
+}
+
+double calculateB(const double x, const double y, const double z)
+{
+    return (sqrt((x * y) / z)) + (pow(cos(pow(x + y, 2)), 2));
 }
